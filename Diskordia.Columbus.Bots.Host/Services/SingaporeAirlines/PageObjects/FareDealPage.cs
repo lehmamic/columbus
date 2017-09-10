f567c70fd5a8e2c7cfc3f08d79b4119781ae1524
Michael Lehmann <lehmamic@bluewin.ch>
2017-09-10 20:56:24 +0200

    feat(SingaporeAirlinesBot): Fill the fare deal detail conditions

---
+++ Diskordia.Columbus.Bots.Host/Services/SingaporeAirlines/PageObjects/FareDealPage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+
+namespace Diskordia.Columbus.Bots.Host.Services.SingaporeAirlines.PageObjects
+{
+	public class FareDealPage
+	{
+		private readonly IWebDriver driver;
+		private readonly Uri uri;
+
+		public FareDealPage(IWebDriver driver, Uri uri)
+		{
+			if (driver == null)
+			{
+				throw new ArgumentNullException(nameof(driver));
+			}
+
+			if (uri == null)
+			{
+				throw new ArgumentNullException(nameof(uri));
+			}
+
+			this.driver = driver;
+			this.uri = uri;
+		}
+
+		public string BookBy
+		{
+			get
+			{
+				IWebElement element = this.driver.FindElements(By.CssSelector(".promotion-choice li"))
+				                  .FirstOrDefault(e => string.Equals(e.FindElement(By.ClassName("title")).Text, "Book\nby", StringComparison.OrdinalIgnoreCase));
+
+				var bookBy = string.Empty;
+				if(element != null)
+				{
+					string bookByDay = element.FindElement(By.ClassName("order")).Text;
+					string bookYear = element.FindElement(By.ClassName("text")).Text;
+
+					bookBy = $"{bookByDay} {bookYear}";
+				}
+
+				return bookBy;
+			}
+		}
+
+		public string OutboundTravelPeriod
+		{
+			get
+			{
+				IWebElement element = this.driver.FindElements(By.CssSelector("dl"))
+				    .FirstOrDefault(e => string.Equals(e.FindElement(By.ClassName("title")).Text, "Outbound travel period", StringComparison.OrdinalIgnoreCase));
+
+				var outboundTravelPeriod = string.Empty;
+				if (element != null)
+				{
+					outboundTravelPeriod = element.FindElement(By.ClassName("value")).Text;
+				}
+
+				return outboundTravelPeriod;
+			}
+		}
+
+		public string TravelCompleteDate
+		{
+			get
+			{
+				IWebElement element = this.driver.FindElements(By.CssSelector("dl"))
+					.FirstOrDefault(e => string.Equals(e.FindElement(By.ClassName("title")).Text, "Travel completion date", StringComparison.OrdinalIgnoreCase));
+
+				var travelCompleteDate = string.Empty;
+				if (element != null)
+				{
+					travelCompleteDate = element.FindElement(By.ClassName("value")).Text;
+				}
+
+				return travelCompleteDate;
+			}
+		}
+
+		public void NavigateTo()
+		{
+			driver.Navigate().GoToUrl(this.uri);
+		}
+	}
+}
--- Diskordia.Columbus.Bots.Host/Services/SingaporeAirlines/SingaporeAirlinesFareDealService.cs
+++ Diskordia.Columbus.Bots.Host/Services/SingaporeAirlines/SingaporeAirlinesFareDealService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using Diskordia.Columbus.Bots.Host.Services.SingaporeAirlines.PageObjects;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace Diskordia.Columbus.Bots.Host.Services.SingaporeAirlines
@@ -15,25 +16,51 @@ namespace Diskordia.Columbus.Bots.Host.Services.SingaporeAirlines
 		{
 			using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
 			{
-				var homePage = new HomePage(driver, new Uri("http://www.singaporeair.com/en_UK/ch/home"));
-				homePage.NavigateTo();
-
-				var fareDealsSection = homePage.Sections.OfType<FareDealsSectionComponent>().SingleOrDefault();
-
-				if (fareDealsSection != null)
+				IEnumerable<FareDeal> availableFareDeals = this.SearchForAvailableFareDeals(driver);
+				foreach (var fareDeal in availableFareDeals.ToArray())
 				{
-					foreach (var fareDeal in fareDealsSection.FareDeals)
+					yield return ReadFareDealDetails(driver, fareDeal);
+				}
+			}
+		}
+
+		private static FareDeal ReadFareDealDetails(ChromeDriver driver, FareDeal fareDeal)
+		{
+			var page = new FareDealPage(driver, fareDeal.Link);
+			page.NavigateTo();
+
+			var outboundTravelPeriod = Regex.Split(page.OutboundTravelPeriod, "to")
+								.Select(p => DateTime.Parse(p.Trim()));
+
+			fareDeal.BookBy = DateTime.Parse(page.BookBy);
+			fareDeal.OutboundStartDate = outboundTravelPeriod.ElementAt(0);
+			fareDeal.OutboundEndDate = outboundTravelPeriod.ElementAt(1);
+			fareDeal.TravelCompleteDate = DateTime.Parse(page.TravelCompleteDate);
+
+
+			return fareDeal;
+		}
+
+		private IEnumerable<FareDeal> SearchForAvailableFareDeals(IWebDriver driver)
+		{
+			var page = new HomePage(driver, new Uri("http://www.singaporeair.com/en_UK/ch/home"));
+			page.NavigateTo();
+
+			var fareDealsSection = page.Sections.OfType<FareDealsSectionComponent>().SingleOrDefault();
+
+			if (fareDealsSection != null)
+			{
+				foreach (var fareDeal in fareDealsSection.FareDeals)
+				{
+					yield return new FareDeal
 					{
-						yield return new FareDeal
-						{
-							Link = new Uri(fareDeal.Link),
-							DepartureAirport = fareDealsSection.DepartureAirport,
-							DestinationAirport = fareDeal.DestinationAirport,
-							Class = fareDeal.Class,
-							Currency = Regex.Match(fareDeal.PriceLabel, @"[A-Z]{3}").Value,
-							Price = Decimal.Parse(Regex.Match(fareDeal.PriceValue, @"\d+(\.\d+)?").Value)
-						};
-					}
+						Link = new Uri(fareDeal.Link),
+						DepartureAirport = fareDealsSection.DepartureAirport,
+						DestinationAirport = fareDeal.DestinationAirport,
+						Class = fareDeal.Class,
+						Currency = Regex.Match(fareDeal.PriceLabel, @"[A-Z]{3}").Value,
+						Price = Decimal.Parse(Regex.Match(fareDeal.PriceValue, @"\d+(\.\d+)?").Value)
+					};
 				}
 			}
 		}
